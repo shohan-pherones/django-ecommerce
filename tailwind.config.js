33ee85660e93c47593325cdd635a088c55db3992
shohan-pherones <shohan.sub.56@gmail.com>
2024-11-12 16:11:10 +0600

    create product model

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/**/*.html", "./products/templates/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
